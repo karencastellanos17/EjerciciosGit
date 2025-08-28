c988be3778c61e67d86f7cd23cd1d952df6194c8
Karenc <karencastellanos832@gmail.com>
2025-08-28 16:25:03 -0500

    cambios de codigo

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
             System.out.println("ESTUDIANTE");
             System.out.println("ESTUDIANTE");
             System.out.println("ESTUDIANTE");
-        }
+            System.out.println("GIT STATUS");
+            System.out.println(" GIT BRANCH");
+            System.out.println("GIT ADD");
+            System.out.println("GIT PUSH ORIGIN");
+            System.out.println("GIT COMMIT -M");
+            System.out.println("GIT CONFIG --LIST");
+            System.out.println("GIT LOG");
+            System.out.println("GIT CLONE");
+            System.out.println("GIT FETCH");
+            System.out.println("GIT SWITCH -");
+
+
+
+    }
     }
