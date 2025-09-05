aa32927f77721c50a6c87cc65305a26ecff49dcd
Karenc <karencastellanos83@gmail.com>
2025-09-05 13:35:01 -0500

    ejercicio dados

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
-
     public static void main(String[] args) {
-            System.out.println("HOLA MUNDO");
-            System.out.println("KAREN JULIETH CASTELLANOS V");
-            System.out.println("ESTUDIANTE");
-            System.out.println("ESTUDIANTE");
-            System.out.println("ESTUDIANTE");
-            System.out.println("ESTUDIANTE");
-            System.out.println("GIT STATUS");
-            System.out.println(" GIT BRANCH");
-            System.out.println("GIT ADD");
-            System.out.println("GIT PUSH ORIGIN");
-            System.out.println("GIT COMMIT -M");
-            System.out.println("GIT CONFIG --LIST");
-            System.out.println("GIT LOG");
-            System.out.println("GIT CLONE");
-            System.out.println("GIT FETCH");
-            System.out.println("GIT SWITCH -");
+        Random aleatorio = new Random();
+        int sumD1=0,sumD2=0,sumD3=0;
 
+        for (int i = 1; i <= 20; i++) {
+            int dado1 = aleatorio.nextInt((6 - 1) + 1) + 1;
+            sumD1 = sumD1 + dado1;
 
+            int dado2 = aleatorio.nextInt((6 - 1) + 1) + 1;
+            sumD2 = sumD2 + dado2;
+
+            int dado3 = aleatorio.nextInt((6 - 1) + 1) + 1;
+            sumD3 = sumD3 + dado3;
+        }
+        System.out.println("La suma del dado 1 es "  + sumD1);
+        System.out.println("La suma del dado 2 es "  + sumD2);
+        System.out.println("La suma del dado 3 es "  + sumD3);
+
+        if (sumD1 > sumD2 && sumD1 > sumD3) {
+            System.out.println("El mejor fue el dado 1 con: " + sumD1);
+            if (sumD2 > sumD3) {
+                System.out.println("El del medio fue el dado 2 con: " + sumD2);
+                System.out.println("El peor fue el dado 3 con: " + sumD3);
+            } else {
+                System.out.println("El del medio fue el dado 3 con: " + sumD3);
+                System.out.println("El peor fue el dado 2 con: " + sumD2);
+            }
+        } else if (sumD2 > sumD1 && sumD2 > sumD3) {
+            System.out.println("El mejor fue el dado 2 con: " + sumD2);
+            if (sumD1 > sumD3) {
+                System.out.println("El del medio fue el dado 1 con: " + sumD1);
+                System.out.println("El peor fue el dado 3 con: " + sumD3);
+            } else {
+                System.out.println("El del medio fue el dado 3 con: " + sumD3);
+                System.out.println("El peor fue el dado 1 con: " + sumD1);
+            }
+        } else if (sumD3 > sumD1 && sumD3 > sumD2) {
+            System.out.println("El mejor fue el dado 3 con: " + sumD3);
+            if (sumD1 > sumD2) {
+                System.out.println("El del medio fue el dado 1 con: " + sumD1);
+                System.out.println("El peor fue el dado 2 con: " + sumD2);
+            } else {
+                System.out.println("El del medio fue el dado 2 con: " + sumD2);
+                System.out.println("El peor fue el dado 1 con: " + sumD1);
+            }
+        }
     }
 }
