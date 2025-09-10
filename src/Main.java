e1279837ed841187a8a46fe3ab6acba97a6beddc
Karenc <karencastellanos83@gmail.com>
2025-09-10 08:49:07 -0500

    Ejercicio 4

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,53 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        menu(scanner);
+        scanner.close();
     }
 
-    public static void menu() {
-
-        Scanner scanner = new Scanner(System.in);
+    public static void menu(Scanner scanner) {
         boolean estado = true;
+
         while (estado) {
             System.out.println("Menu");
             System.out.println(" ---------------------------");
             System.out.println("1. Dados 2");
             System.out.println("2. Dados 3");
             System.out.println("3. IMC");
-            System.out.println("4. Salir");
+            System.out.println("4. Promedio de notas");
+            System.out.println("5. Ejercicio nuevo)");
+            System.out.println("6. Salir");
             System.out.println(" ---------------------------");
+            System.out.print("Elija la opcion que desea: ");
 
-            System.out.println("Eliga la opcion que desea");
-            int OPC = scanner.nextInt();
+            int opc = scanner.nextInt();
 
-            switch (OPC) {
+            switch (opc) {
                 case 1:
                     dados2();
-
                     break;
-
                 case 2:
                     dados3();
-
                     break;
-
                 case 3:
-                    imc();
-
+                    imc(scanner);
                     break;
-
                 case 4:
-                default:
+                    promedioNotas(scanner);
+                    break;
+                case 5:
+                    System.out.println("Ejercicio nuevo");
+                    break;
+                case 6:
                     estado = false;
+                    System.out.println("Gracias por usar");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
             }
+
+            System.out.println();
         }
     }
 
@@ -52,20 +60,18 @@ public class Main {
         int sumD2 = 0;
 
         for (int i = 1; i <= 20; i++) {
-            int dado1 = aleatorio.nextInt((6 - 1) + 1) + 1;
-            sumD1 = sumD1 + dado1;
-
-            int dado2 = aleatorio.nextInt((6 - 1) + 1) + 1;
-            sumD2 = sumD2 + dado2;
-
+            sumD1 += aleatorio.nextInt(6) + 1;
+            sumD2 += aleatorio.nextInt(6) + 1;
         }
-        System.out.println(sumD1);
-        System.out.println(sumD2);
+        System.out.println("La suma del dado 1 es " + sumD1);
+        System.out.println("La suma del dado 2 es " + sumD2);
 
         if (sumD1 > sumD2) {
-            System.out.println("Gana el dado 1");
+            System.out.println("Gano el dado 1");
+        } else if (sumD2 > sumD1) {
+            System.out.println("Gano el dado 2");
         } else {
-            System.out.println("Gana el dado 2");
+            System.out.println("Empataron");
         }
     }
 
@@ -74,83 +80,65 @@ public class Main {
         int sumD1 = 0, sumD2 = 0, sumD3 = 0;
 
         for (int i = 1; i <= 20; i++) {
-            int dado1 = aleatorio.nextInt((6 - 1) + 1) + 1;
-            sumD1 = sumD1 + dado1;
-
-            int dado2 = aleatorio.nextInt((6 - 1) + 1) + 1;
-            sumD2 = sumD2 + dado2;
-
-            int dado3 = aleatorio.nextInt((6 - 1) + 1) + 1;
-            sumD3 = sumD3 + dado3;
+            sumD1 += aleatorio.nextInt(6) + 1;
+            sumD2 += aleatorio.nextInt(6) + 1;
+            sumD3 += aleatorio.nextInt(6) + 1;
         }
         System.out.println("La suma del dado 1 es " + sumD1);
         System.out.println("La suma del dado 2 es " + sumD2);
         System.out.println("La suma del dado 3 es " + sumD3);
+    }
 
-        if (sumD1 > sumD2 && sumD1 > sumD3) {
-            System.out.println("El mejor fue el dado 1 con: " + sumD1);
-            if (sumD2 > sumD3) {
-                System.out.println("El del medio fue el dado 2 con: " + sumD2);
-                System.out.println("El peor fue el dado 3 con: " + sumD3);
-            } else {
-                System.out.println("El del medio fue el dado 3 con: " + sumD3);
-                System.out.println("El peor fue el dado 2 con: " + sumD2);
-            }
-        } else if (sumD2 > sumD1 && sumD2 > sumD3) {
-            System.out.println("El mejor fue el dado 2 con: " + sumD2);
-            if (sumD1 > sumD3) {
-                System.out.println("El del medio fue el dado 1 con: " + sumD1);
-                System.out.println("El peor fue el dado 3 con: " + sumD3);
-            } else {
-                System.out.println("El del medio fue el dado 3 con: " + sumD3);
-                System.out.println("El peor fue el dado 1 con: " + sumD1);
-            }
-        } else if (sumD3 > sumD1 && sumD3 > sumD2) {
-            System.out.println("El mejor fue el dado 3 con: " + sumD3);
-            if (sumD1 > sumD2) {
-                System.out.println("El del medio fue el dado 1 con: " + sumD1);
-                System.out.println("El peor fue el dado 2 con: " + sumD2);
-            } else {
-                System.out.println("El del medio fue el dado 2 con: " + sumD2);
-                System.out.println("El peor fue el dado 1 con: " + sumD1);
-            }
+    public static void imc(Scanner scanner) {
+        System.out.print("Digite el peso en (Kg): ");
+        double peso = scanner.nextDouble();
+
+        System.out.print("Digite su estatura en (M) con coma: ");
+        double estatura = scanner.nextDouble();
+
+        double indice = peso / Math.pow(estatura, 2);
+        System.out.println("Su índice de masa corporal (IMC) es: " + indice);
+
+        if (indice < 18.5) {
+            System.out.println("Su situacion de indice es de: bajo peso");
+        } else if (indice >= 18.5 && indice <= 24.9) {
+            System.out.println("Su situacion de indice es de: peso normal");
+        } else if (indice >= 25 && indice <= 26.9) {
+            System.out.println("Su situacion de indice es de: sobrepeso grado 1");
+        } else if (indice >= 27 && indice <= 29.9) {
+            System.out.println("Su situacion de indice es de: sobrepeso grado 2");
+        } else if (indice >= 30 && indice <= 34.9) {
+            System.out.println("Su situacion de indice es de: obesidad tipo 1");
+        } else if (indice >= 35 && indice <= 39.9) {
+            System.out.println("Su situacion de indice es de: obesidad tipo 2");
+        } else if (indice >= 40 && indice <= 49.9) {
+            System.out.println("Su situacion de indice es de: obesidad tipo 3 mórbida");
+        } else if (indice >= 50) {
+            System.out.println("Su situacion de indice es de: obesidad tipo 4 extrema");
         }
     }
 
-    public static String imc() {
-        double peso;
+    public static void promedioNotas(Scanner scanner) {
+        System.out.println("Promedio de notas");
+        System.out.println("----------------------------");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("----------------------");
-        System.out.println("Digite el peso en (Kg)");
-        peso = scanner.nextDouble();
+        int nota1, nota2, nota3, nota4, nota5;
 
-        double estatura;
-        System.out.println("----------------------");
-        System.out.println("Digite su estatura en (M)");
-        estatura = scanner.nextDouble();
+        System.out.print("Digite la primer nota: ");
+        nota1 = scanner.nextInt();
+        System.out.print("Digite la segunda nota: ");
+        nota2 = scanner.nextInt();
+        System.out.print("Digite la tercera nota: ");
+        nota3 = scanner.nextInt();
+        System.out.print("Digite la cuarta nota: ");
+        nota4 = scanner.nextInt();
+        System.out.print("Digite la quinta nota: ");
+        nota5 = scanner.nextInt();
 
+        double promedio = (nota1 + nota2 + nota3 + nota4 + nota5) /5.0;
 
-        double indice = peso / Math.pow(estatura, 2);
-
-        if (indice < 18.5) {
-            return "Su situacion de indice es de: bajo paso";
-        } else if (indice >= 18.5 && indice <= 24.9) {
-            return"Su situacion de indice es de: peso normal";
-        } else if (indice >= 25 && indice <= 26.9) {
-            return "Su situacion de indice es de: sobrepeso grado 1";
-        } else if (indice >= 27 && indice <= 29.9) {
-            return "Su situacion de indice es de: sobrepeso grado 2";
-        } else if (indice >= 30 && indice <= 34.9) {
-            return "Su situacion de indice es de: obesidad tipo 1";
-
-        } else if (indice >= 35 && indice <= 39.9) {
-            return "Su situacion de indice es de: obesidad tipo 2";
-        } else if (indice >= 40 && indice <= 49.9) {
-            return "Su situacion de indice es de: obesidad tipo 3";
-        } else {
-            return "Obesidad tipo 4 extrema";
-        }
+        System.out.println("---------------------------------");
+        System.out.println("El promedio es: " + promedio);
 
     }
 }
